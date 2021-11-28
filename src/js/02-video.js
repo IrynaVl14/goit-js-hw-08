76c88571a9b878956fa2dc36cdf361c2fe77b363
Iryna Vlasenko <via-lilly@ukr.net>
2021-11-28 19:50:19 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import throttle from "lodash.throttle";
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const currentTime = localStorage.getItem("videoplayer-current-time");
+
+player.on('timeupdate', throttle(onTimeupdate, 1000));
+
+function onTimeupdate(data) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds));
+}
+
+player.setCurrentTime(currentTime).then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+
 
