90d93f4ee9a0470a6e54ec13512f75b7eb575ff1
Iryna Vlasenko <via-lilly@ukr.net>
2021-11-28 12:58:09 +0200

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const makeListOfImages = ({ preview, original, description }) => {
+  return `
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}"/>
+        </a>       
+    `    
+};
+
+const listOfImages = galleryItems.map(makeListOfImages).join('');
+const galleryEl = document.querySelector('div.gallery');
+galleryEl.insertAdjacentHTML('afterbegin', listOfImages);
+
+let lightbox = new SimpleLightbox('.gallery a');
+
+galleryEl.addEventListener('click', onGalleryClick)
+
+function onGalleryClick(evt) {
+    evt.preventDefault();
+    const isImgEl = evt.target.classList.contains('gallery__image');
+
+    if (!isImgEl) {
+        return;
+    }
+
+    lightbox.options.captionsData = 'alt';       
+    lightbox.options.captionDelay = 250;
+    
+}
